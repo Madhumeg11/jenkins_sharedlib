c8189a7a9b78de99fdb1ded2da8fef30c6d3a4ce
Madhumeg11 <itsrenukamma10@gmail.com>
2025-11-21 10:28:02 +0530

    Create gitCheckout.groovy

---
+++ vars/gitCheckout.groovy
@@ -0,0 +1,11 @@
+def call(String repoUrl, String branch = null, String credentialId = null) {
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: branch]],
+        userRemoteConfigs: [[
+            url: repoUrl,
+            credentialsId: credentialId
+        ]]
+    ])
+}
+#call(repoUrl, branch, credentialId)
